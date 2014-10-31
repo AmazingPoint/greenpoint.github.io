08ebb79495f28c184b1836e11323d045e27dc205
GreenPointgo <greenpointan@icloud.com>
2014-10-31 19:11:56 +0800

    add grouplist and userlist in index

--- osdj/osb/views.py
+++ osdj/osb/views.py
@@ -14,9 +14,17 @@ from osb.models import *
 from django.db.models import Count
 
 def index(request):
-	#获取用户最喜欢的三个话题
-	topics_list = Topics.objects.\
+	#获取用户最喜欢的8个话题
+	#get 7 top topics 
+	top_topics_list = Topics.objects.\
 			annotate(num_likeduser=Count('likeduser')).\
 			order_by('-num_likeduser')[0:8]
+	#获取最新加入的7个用户
+	#get 7 new user
 	recent_user_list = User.objects.order_by('-id')[0:7]
+	#获取成员最多的7个小组
+	#get 7 group which have more member
+	top_group_list = Group.objects.\
+			annotate(num_member=Count('member')).\
+			order_by('-num_member')[0:7]
 	return render(request, 'osb/index.html', locals())
